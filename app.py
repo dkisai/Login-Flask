8d0d342824ddd63284b786268e447708e471b5ce
dKisai <diego.kisai@live.com>
2022-05-24 16:29:29 -0500

    Add: css and js files

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, app, render_template, request, redirect, url_for, flash, jsonify
 
-
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
+    
