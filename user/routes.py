0b96fafc4118e6932b937b2f9bf550ac4b7f2b40
dKisai <diego.kisai@live.com>
2022-05-24 23:48:20 -0500

    Ready

--- user/routes.py
+++ user/routes.py
@@ -2,7 +2,14 @@ from flask import Flask
 from app import app
 from user.models import User
 
-@app.route('/users/signup/', methods=['GET'])
+@app.route('/user/signup', methods=['POST'])
 def signup():
-    user = User()
-    return user.singup()
+  return User().signup()
+
+@app.route('/user/signout')
+def signout():
+  return User().signout()
+
+@app.route('/user/login', methods=['POST'])
+def login():
+  return User().login()
